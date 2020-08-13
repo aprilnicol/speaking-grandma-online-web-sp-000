f7f4dedf574fdc35c0ab815aba9df9d5a8a46466
April Adams <illusionlace@gmail.com>
2020-08-13 04:07:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,7 +11,7 @@ def speak_to_grandma(phrase)
 if phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN"
 elsif phrase != phrase.upcase()
-  "HUH?! SPEAK UP SONNY!"
+  "HUH?! SPEAK UP, SONNY!"
 else
     phrase == phrase.upcase()
   return "NO, NOT SINCE 1938!"
