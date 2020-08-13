68e39a9f7fdc534d08460c4e77a54020368ef4ca
April Adams <illusionlace@gmail.com>
2020-08-13 04:03:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,7 +8,7 @@ def speak_to_grandma(phrase)
 # NO, NOT SINCE 1938!
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
-if phrase == phrase.upcase()
+if phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN"
 elsif phrase != phrase.upcase()
 return "NO, NOT SINCE 1938!"
